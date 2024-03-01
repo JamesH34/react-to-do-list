cf0930c2b1048e480db93d816ac9c413037d2040
James Hershey <jameshershey34@gmail.com>
2024-03-01 09:52:33 -0500

    functioning app, needs delete button

--- src/App.jsx
+++ src/App.jsx
@@ -1,67 +1,72 @@
-import { useState} from 'react'
+import { useState } from 'react'
 import './App.css'
-import taskData from '../tasks.json';
+import taskData from '../tasks.json';//data from json file is represented by "taskData"
 
 function App() {
-  const [items, setItems] = useState([]);//sets items as an empty array
-  const [currentInput, setCurrentInput] = useState(''); //currentInput will hold the current value. setCurrent will hold updated state
-  const [selectedTask, setSelectedTask] = useState([]);//tracking the checkbox as an array
-  const [tasks, setTasks] = useState(taskData)
+  const [currentInput, setCurrentInput] = useState(''); //currentInput will hold the current value. setCurrent will update the state
+  const [tasks, setTasks] = useState(taskData);//tasks variable is now holding info from 'taskData'
 
 
-  const renderTask = (task) => {
-    return `${task.id}, ${task.description}, COMPLETED: ${task.completed ? "Yes" : "No"}`;
+  const renderTask = (item) => {
+    return `${item.id}, ${item.description}, COMPLETED: ${item.completed ? "Yes" : "No"}`;
   }
-  
-  
+
+
   const handleInputChange = (event) => {//triggererd when the user types in the input field
     setCurrentInput(event.target.value);//event=typing, target=input field where it's called, value=what the user types
   };
 
-  const addTask = () => {
-    //handle empty inputs
-    // event.preventDefault(); // Prevent the default form submission behavior
-    const newTask = {id: tasks.length + 1, 
-    description: currentInput,
-    completed: false}
+  const addTask = (event) => {
+    event.preventDefault(); // Prevent the default form submission behavior/page refresh
+    const newTask = {
+      id: tasks.length + 1,
+      description: currentInput,
+      completed: false
+    }//false makes sure 'completed' is initially set to 'no'
     setTasks([...tasks, newTask]); //makes an array with all current values then add the current input to the array
     setCurrentInput(''); // Clear the input field after adding the item
   };
 
 
-  // const toggleTaskSelection = (index) => {
-  //   const currentIndex = selectedTask.indexOf(index);//get index of selected task
-  //   let newSelectedTask = [...selectedTask];//make a copy of selectedTask array to modify
-    
-  //   if (currentIndex === -1) {//check if the current index is in the array and adds it if it isnt
-  //     newSelectedTask.push(index);
-  //   } else {
-  //     newSelectedTask.splice(currentIndex, 1);//if item was selected, use splice to remove it
-  //   }
-  //   setSelectedTask(newSelectedTask);//update the selected tasks to the new array.
-  // };
+  const changeStatus = (id) => {
+    setTasks(tasks.map(item => {//iterate over the array of tasks
+      if (item.id === id) {//while iterating, check if the id matches the id of the clicked item
+        return { ...item, completed: !item.completed };//if it matches return a new object with the "completed" changed to the opposite
+      }
+      return item;//if it doesnt match return the original task
+    }));
+  };
 
   return (
     <>
-   
-      <h1>To Do List</h1>
-      <div className="card">
-        <form>
-        <input id='newTask' 
-        name='task' 
-        type='text' 
-        value={currentInput}
-        onChange={handleInputChange//calls the function and makes .target reference here
-        }></input>
-        <button type = 'button' onClick={addTask} > Add Task </button>
-        <ul id='listOfStuff' name='stuff'>
-          {tasks.map((task, index) => <li key = {index}>{renderTask(task)}</li>)}
 
-         
-        </ul>
+      <h1>To Do List</h1>
+      <div id='listContainer'>
+
+        <form>
+          <input id='newTask'
+            name='task'
+            type='text'
+            value={currentInput}
+            onChange={handleInputChange//calls the function and makes .target reference here
+            }></input>
+          <button type='submit' onClick={addTask} > Add Task </button>
         </form>
+
+        <ul id='listOfStuff' name='stuff'>
+          {tasks.map((item) => (//go through the array of tasks and perform the following function 
+            //to make them into list items
+            // li represents each task. when clicked calls changeStatus with the id as a reference
+            //key is needed for react to keep track of list items by id
+            <li onClick={() => changeStatus(item.id)} key={item.id}>
+              {renderTask(item)}
+            </li>
+          ))}
+
+
+        </ul>
       </div>
-      <p className="read-the-docs">
+      <p>
         Don't click <a href='https://www.youtube.com/watch?v=dQw4w9WgXcQ'>here</a>
       </p>
     </>
@@ -69,3 +74,8 @@ function App() {
 }
 
 export default App
+
+
+
+
+//to do- handle empty input field, add delete button, add delete functionality
